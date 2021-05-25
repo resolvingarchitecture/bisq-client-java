06c5b3e3584ef22717b53ca36c3156400577a62c
objectorange <brian@resolvingarchitecture.io>
2021-05-25 09:26:17 -0400

    no longer pursued

--- src/main/java/ra/bisq/Bisq.java
+++ src/main/java/ra/bisq/Bisq.java
@@ -47,44 +47,6 @@ public interface Bisq {
     void withdrawal(Envelope envelope);
 
     /**
-     * Request an Exchange from/to a currency to/from BTC
-     * using a supported method for a specified amount in BTC.
-     *
-     * Request Envelope Requires in NVP:
-     *      toCurrency:String (e.g. BTC, USD, EUR) - Required
-     *      fromCurrency: String (e.g. BTC, USD, EUR) - Required
-     *      amountSats:Long - Required
-     *
-     * Supported methods:
-     * LBP : F2F
-     * USD : Zelle
-     * BSQ : Altcoins
-     *
-     * Buy BTC as Taker is as follows:
-     *  1. Look for Buy BTC offer for requested currency, amount, and method.
-     *  2. If offer not found
-     *      2.a. Continue to Buy BTC as Maker
-     *  3. If offer found,
-     *      3.a. Does current BSQ balance cover Taker costs?
-     *          3.a.1. Yes
-     *              3.a.1.a. Take Offer receiving Trade
-     *              3.a.1.b. Wait for verification that BTC sent to Escrow
-     *              3.a.1.c. Verify funds sent to Seller
-     *              3.a.1.d. Wait for BTC funds released to provided address
-     *              3.a.1.e. Close out Trade
-     *          3.a.2. No
-     *              3.a.2.a. Sell BTC for BSQ to meet minimum
-     *              3.a.2.b. Start back over at step 1.
-     *
-     * Buy BTC as Maker is as follows:
-     *  1.
-     *
-     * Sell BTC as Taker is as follows:
-     *  1.
-     *
-     * Sell BTC as Maker is as follows:
-     *  1.
-     *
      *
      *
      * @param envelope
